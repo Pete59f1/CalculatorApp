2cdf8f14211eec7a19621baca0f8a33605b4a266
Pete59f1 <pete59f1@edu.eal.dk>
2018-10-08 14:19:54 +0200

    Ændret path

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -18,7 +18,7 @@ namespace CalculatorApp
         private void Run()
         {
             SmartMenu menu = new SmartMenu();
-            menu.LoadMenu("MenuSpec.txt");
+            menu.LoadMenu("CalcMenu.txt");
             menu.Activate();
         }
     }
