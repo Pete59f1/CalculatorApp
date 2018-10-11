40f46ae09a7361008c8c8a3da00721435c1d4366
Niclas Holmquist Jensen <nicl2445@edu.eal.dk>
2018-10-11 16:00:38 +0200

    fix af fejl

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -18,7 +18,7 @@ namespace CalculatorApp
         private void Run()
         {
             SmartMenu menu = new SmartMenu();
-            menu.LoadMenu("CalcMenu.txt");
+            menu.LoadMenu(@"..\..\CalcMenu.txt");
             menu.Activate();
         }
     }
